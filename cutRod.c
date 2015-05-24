3ddf42a7b0394a28495e42745cd9cb529a59f181
RishabhPuri <rishabhpuri@gmail.com>
2015-05-24 19:21:30 +0530

    Update cutRod.c

--- cutRod.c
+++ cutRod.c
@@ -41,7 +41,7 @@ unsigned int cutRodDp1 (unsigned int memo[], unsigned int len1)
 
 /** bottom - up approach , solve smaler subproblems for a given problem */
 //unsigned int price[11] = {1,5,8,9,10,17,17,20,24,30};
-unsigned int cutRodDp2(unsigned int memo[], unsigned int len1)
+unsigned int cutRodDp2(unsigned int optSize[], unsigned int memo[], unsigned int len1)
 {
   unsigned int maxVal=0, i , j;
   /** think: the subproblem approach required me to think , that to solve a problem i should have solved
@@ -55,9 +55,10 @@ unsigned int cutRodDp2(unsigned int memo[], unsigned int len1)
     {
       maxVal = max(maxVal, (price[i]+memo[j-i-1]));
       count++;
+      optSize[j] = i;
     }
     memo[j] = maxVal;
-    printf ("memo %d, len1 %d \n", memo[j], j);
+    //printf ("memo %d, len1 %d \n", memo[j], j);
   }
   return memo[len1];
 }
@@ -67,10 +68,16 @@ void main (int argc , char **argv)
 
    unsigned int len1=atoi(argv[1]), len2=0, maxRev1 = 0,maxRev = 0, i ;
    unsigned int memo[11] =  {0};
+   unsigned int optSize[11] =  {0};
    //maxRev= cutRod(price[len1-1], len1);
    //maxRev = cutRodDp1(memo, len1);
-   maxRev = cutRodDp2(memo, len1);
+   maxRev = cutRodDp2(optSize, memo, len1);
    printf ("Max revenue is %d count %d\n", maxRev, count);
+   for (i=0; i<len1;++i)
+   {
+     printf ("length %d, max-revenue %d, firstCut %d\n", i, memo[i], optSize[i]);
+   }
+
 }
 
 
